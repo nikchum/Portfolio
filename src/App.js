8db1a96736d97b25e20a657ff180673b685bd50a
nikchum <nchumak94@gmail.com>
2023-03-05 05:38:47 +0200

    fix Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/main.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
